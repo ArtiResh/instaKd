d9b25755151602dd5f46ae1942cb7b66f9c86808
Aleksey <alexkoss93@mail.ru>
2015-11-24 00:51:39 +0200

    Main page head done

--- index.php
+++ index.php
@@ -4,6 +4,40 @@ $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/js/fancybox/jquery.fancybo
 $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/js/fancybox/helpers/jquery.fancybox-thumbs.css");
 $APPLICATION->SetAdditionalCSS("/promo/instagram/css/styles.css");
 ?>
+<div class="promo_head">
+    <div class="promo_logo"><img src="images/main/hashtag_big.png" /></div>
+    <div class="promo_menu">
+        <a href="#users" class="users underline">Участники</a>
+        <a href="#rules" class="rules underline">Условия</a>
+        <a href="#prizes" class="prizes underline">Призы</a>
+    </div>
+</div>
+<div class="promo_desc">
+    <div class="promo_desc_title">Примите участие в нашем селфи-квесте в
+        <img src="images/main/instagram_full_logo.png" /> и сделайте свой Новый Год незабываемым!
+    </div>
+    <div class="promo_steps">
+        <div class="step">
+            <div class="circle"><img src="images/main/head_menu_1.png" /></div>
+            <div class="step_desc"><div class="sub_desc underline">Подпишитесь на</div><div class="sub_desc underline">наш Instagram</div></div>
+        </div>
+        <span class="arrow"></span>
+        <div class="step">
+            <div class="circle"><img src="images/main/head_menu_2.png" /></div>
+            <div class="step_desc"><div class="sub_desc underline">Выполняйте</div><div class="sub_desc underline">задания квеста</div></div>
+        </div>
+        <span class="arrow"></span>
+        <div class="step">
+            <div class="circle"><img src="images/main/head_menu_3.png" /></div>
+            <div class="step_desc"><div class="sub_desc underline">Набирайте лайки</div><div class="sub_desc underline">под своим фото</div></div>
+        </div>
+        <span class="arrow"></span>
+        <div class="step">
+            <div class="circle"><img src="images/main/head_menu_4.png" /> </div>
+            <div class="step_desc"><div class="sub_desc underline">Выиграйте iPhone 6S</div><div class="sub_desc underline">и кучу других призов</div></div>
+        </div>
+    </div>
+</div>
 <div class="inst_content"></div>
 <div class="main_top_photos main_photos"></div>
 <div class="main_bot_photos main_photos"></div>
